e34eb8ea99703ada9bb670bf524d1d728a2c5558
emersonjuniorl <134564419+emersonjuniorl@users.noreply.github.com>
2025-01-02 22:53:12 -0300

    Add funcionalidades

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/features')
+def features():
+    return render_template('features.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
