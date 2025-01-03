b369f31cd07684c0bb0b3fa764c1d043c2f949ce
emersonjuniorl <134564419+emersonjuniorl@users.noreply.github.com>
2025-01-03 17:01:19 -0300

    Atualização de texto e imagens

--- app.py
+++ app.py
@@ -6,9 +6,5 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/features')
-def features():
-    return render_template('features.html')
-
 if __name__ == '__main__':
     app.run(debug=True)
